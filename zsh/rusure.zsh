e1f523bc8a0ef2ba45bcbd60195d35c1714832fa
nakahira <1021057927@qq.com>
2022-01-03 18:41:54 +0800

    add logo

--- zsh/rusure.zsh
+++ zsh/rusure.zsh
@@ -1,19 +1,19 @@
 #!/bin/zsh
 
-config_path=${0:A:h}/config.yml
-cmds=("rm -rf /" "git checkout ." "git co ." "xzp")
-ask="This is a dangerous operation,are you sure?"
-yes='Y'
-no='N'
+readonly _rus_config_path=${0:A:h}/config.yml
+readonly _rus_cmds=("rm -rf /" "git checkout ." "git co ." "xzp")
+readonly _rus_ask="This is a dangerous operation,are you sure?"
+readonly _rus_yes='Y'
+readonly _rus_no='N'
 
 function rusure_exec(){
 	if [[ $# == 0 ]]; then
 		return
 	fi
-    printf "${ask}(${yes}/${no}):"
+    printf "${_rus_ask}(${_rus_yes}/${_rus_no}):"
     read result
-    
-    if [[ ${result:l} == 'y' ]] || [[ ${result:l} == 'yes' ]]; then
+
+    if [[ ${result:l} == 'y' ]] || [[ ${result:l} == '_rus_yes' ]]; then
     	eval $*
     else
     	echo 'canced!'
@@ -26,7 +26,7 @@ function resure_accept_line() {
 		return
 	fi
 	matched='false'
-	for cmd in ${cmds}; do
+	for cmd in ${_rus_cmds}; do
 		if [[ "${cmd}" == "${BUFFER}" ]]; then
 			matched='true'
 			break
